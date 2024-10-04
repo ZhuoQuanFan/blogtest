d12ddca1d934f1bb36e1d36aa18befe503b8b2cf
范倬铨 <1310195843@qq.com>
2024-10-04 12:07:22 +0800

    add svgs

--- src/components/blocks/skills/index.tsx
+++ src/components/blocks/skills/index.tsx
@@ -3,22 +3,24 @@ import type { StaticImageData } from 'next/image'
 
 import { Block } from '@/components/blocks/block'
 import cssSvg from '@/images/logo/css.svg'
-import gatsbySvg from '@/images/logo/gatsby.svg'
-import graphqlSvg from '@/images/logo/graphql.svg'
+// import gatsbySvg from '@/images/logo/gatsby.svg'
+// import graphqlSvg from '@/images/logo/graphql.svg'
 import htmlSvg from '@/images/logo/html.svg'
 import javascriptSvg from '@/images/logo/javascript.svg'
-import nestjsSvg from '@/images/logo/nestjs.svg'
-import nextjsSvg from '@/images/logo/nextjs.svg'
+// import nestjsSvg from '@/images/logo/nestjs.svg'
+// import nextjsSvg from '@/images/logo/nextjs.svg'
 import nodejsSvg from '@/images/logo/nodejs.svg'
-import prismaSvg from '@/images/logo/prisma.svg'
+// import prismaSvg from '@/images/logo/prisma.svg'
 import reactSvg from '@/images/logo/react.svg'
-import reactNativeSvg from '@/images/logo/react-native.svg'
-import swiftSvg from '@/images/logo/swift.svg'
-import tailwindCSSSvg from '@/images/logo/tailwind-css.svg'
-import typeScriptSvg from '@/images/logo/typescript.svg'
+// import reactNativeSvg from '@/images/logo/react-native.svg'
+// import swiftSvg from '@/images/logo/swift.svg'
+// import tailwindCSSSvg from '@/images/logo/tailwind-css.svg'
+// import typeScriptSvg from '@/images/logo/typescript.svg'
 import vueSvg from '@/images/logo/vue.svg'
-import webpackSvg from '@/images/logo/webpack.svg'
-
+// import webpackSvg from '@/images/logo/webpack.svg'
+import javaSvg from '@/images/logo/java-svgrepo-com.svg'
+import PythonSvg from '@/images/logo/python-svgrepo-com.svg'
+import SqlSvg from '@/images/logo/mysql-svgrepo-com (1).svg'
 const Galton = dynamic(() => import('./galton').then(module => module.Galton), {
   ssr: false,
 })
@@ -27,19 +29,12 @@ const images: StaticImageData[] = [
   htmlSvg,
   cssSvg,
   javascriptSvg,
-  typeScriptSvg,
   reactSvg,
-  tailwindCSSSvg,
-  nextjsSvg,
-  gatsbySvg,
   vueSvg,
   nodejsSvg,
-  reactNativeSvg,
-  webpackSvg,
-  swiftSvg,
-  nestjsSvg,
-  prismaSvg,
-  graphqlSvg,
+  javaSvg,
+  PythonSvg,
+  SqlSvg
 ]
 export const Skills = () => {
   return (

b979f82b24571bcf0162bae7573a79efd04ed1d6
范倬铨 <1310195843@qq.com>
2024-10-08 01:31:09 +0800

    add dependencies

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,18 +1,26 @@
 import { Avatar } from '@/components/blocks/avatar'
 import { Bio } from '@/components/blocks/bio'
-import { Font } from '@/components/blocks/font'
+// import { Font } from '@/components/blocks/font'
 import { Github } from '@/components/blocks/github'
 // import { Juejin } from '@/components/blocks/juejin'
 import { Pinned } from '@/components/blocks/pinned'
 import { Posts } from '@/components/blocks/posts'
 import { Resume } from '@/components/blocks/resume'
 import { Skills } from '@/components/blocks/skills'
+import { Block } from '@/components/blocks/block'
 import { Tags } from '@/components/blocks/tags'
 import { ThemeToggle } from '@/components/blocks/theme-toggle'
 import { Grid } from '@/components/grid'
 import { Header } from '@/components/header'
 import { Csdn } from '@/components/blocks/csdn'
-import { Calendar } from '@nextui-org/react'
+// import { Cal } from '@/components/blocks/Calendar'
+import { CalendarBlock} from '@/components/blocks/Calendar';
+
+// // import {today, getLocalTimeZone} from "@internationalized/date";
+// import dayjs from 'dayjs'
+// import 'dayjs/locale/zh-cn'
+// dayjs.locale('zh-cn')
+
 export default function Layout() {
   return (
     <>
@@ -22,7 +30,9 @@ export default function Layout() {
         <Avatar />
         <Skills />
         <Pinned />
-        <Font />
+        {/*<Font />*/}
+        {/*<Cal />*/}
+        <CalendarBlock />
         <Resume />
         <ThemeToggle />
         <Tags />
@@ -30,8 +40,10 @@ export default function Layout() {
         <Github />
         <Csdn />
         {/*<Juejin />*/}
-        <Calendar />
+
       </Grid>
+
     </>
+
   )
 }
---
+++ src/components/blocks/Calendar.tsx
@@ -0,0 +1,11 @@
+import {Block} from "@/components/blocks/block";
+import {Calendar} from "antd";
+
+export const CalendarBlock = () => {
+  return (
+      <Block  className='flex items-stretch justify-stretch overflow-clip  bg-surface outline-offset-4 max-md:col-span-2' data-type='posts'>
+
+          <Calendar fullscreen={false} />
+      </Block>
+  )
+};

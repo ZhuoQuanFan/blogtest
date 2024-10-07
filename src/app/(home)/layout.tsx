baf99ee63a3598160dfa41c44a092b7200f5970c
范倬铨 <1310195843@qq.com>
2024-10-08 01:52:05 +0800

    add dependencies

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,5 +1,8 @@
+// import { Cal } from '@/components/blocks/Calendar'
+import { CalendarBlock} from '@/components/blocks/Calendar';
 import { Avatar } from '@/components/blocks/avatar'
 import { Bio } from '@/components/blocks/bio'
+import { Csdn } from '@/components/blocks/csdn'
 // import { Font } from '@/components/blocks/font'
 import { Github } from '@/components/blocks/github'
 // import { Juejin } from '@/components/blocks/juejin'
@@ -7,14 +10,11 @@ import { Pinned } from '@/components/blocks/pinned'
 import { Posts } from '@/components/blocks/posts'
 import { Resume } from '@/components/blocks/resume'
 import { Skills } from '@/components/blocks/skills'
-import { Block } from '@/components/blocks/block'
+// import { Block } from '@/components/blocks/block'
 import { Tags } from '@/components/blocks/tags'
 import { ThemeToggle } from '@/components/blocks/theme-toggle'
 import { Grid } from '@/components/grid'
 import { Header } from '@/components/header'
-import { Csdn } from '@/components/blocks/csdn'
-// import { Cal } from '@/components/blocks/Calendar'
-import { CalendarBlock} from '@/components/blocks/Calendar';
 
 // // import {today, getLocalTimeZone} from "@internationalized/date";
 // import dayjs from 'dayjs'
--- src/components/blocks/Calendar.tsx
+++ src/components/blocks/Calendar.tsx
@@ -1,11 +1,24 @@
-import {Block} from "@/components/blocks/block";
-import {Calendar} from "antd";
+import { Calendar, ConfigProvider } from 'antd'
+
+import { Block } from '@/components/blocks/block'
 
 export const CalendarBlock = () => {
   return (
-      <Block  className='flex items-stretch justify-stretch overflow-clip  bg-surface outline-offset-4 max-md:col-span-2' data-type='posts'>
-
-          <Calendar fullscreen={false} />
-      </Block>
+    <Block
+      className='flex items-stretch justify-stretch overflow-clip  bg-surface outline-offset-4 max-md:col-span-2'
+      data-type='posts'
+    >
+      <ConfigProvider
+        theme={{
+          components: {
+            Calendar: {
+              /* 这里是你的组件 token */
+              fullPanelBg: '#000000',
+            },
+          },
+        }}
+      ></ConfigProvider>
+      <Calendar fullscreen={false} />
+    </Block>
   )
-};
+}

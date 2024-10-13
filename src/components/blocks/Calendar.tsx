5b038c1395cccbaece58510cdc4dddf3d0031345
ZhuoQuanFan <fanzhuoquan@gmail.com>
2024-10-13 10:57:24 +0800

    modify style suitable for phone

--- src/components/blocks/Calendar.tsx
+++ src/components/blocks/Calendar.tsx
@@ -1,14 +1,29 @@
 import { Calendar } from 'antd'
 
+// import React from 'react'
+
 import { Block } from '@/components/blocks/block'
 
 export const CalendarBlock = () => {
   return (
     <Block
-      className='flex items-stretch justify-stretch overflow-clip  bg-surface outline-offset-4 max-md:col-span-2 '
-      data-type='posts'
+      className='flex items-stretch justify-stretch overflow-clip  bg-surface outline-offset-4 max-md:col-span-2 max-md:row-span-2'
+      data-type='calendar'
     >
-      <Calendar fullscreen={false} />
+      <div>
+        <Calendar fullscreen={false} />
+      </div>
+    </Block>
+  )
+}
+
+export const BiggerCalendar = () => {
+  return (
+    <Block
+      className='col-span-2 flex flex-col overflow-clip  from-amber-50 to-surface dark:from-surface-1 max-lg:row-span-2 sm:col-span-3 md:col-span-2 lg:col-span-4'
+      data-type='calendar-small'
+    >
+      <Calendar />
     </Block>
   )
 }

b3a280e156b8cfe0e89d996bc86f1a7f7deb450b
ZhuoQuanFan <fanzhuoquan@gmail.com>
2024-10-20 12:27:17 +0800

    update latex

--- src/app/(article)/layout.tsx
+++ src/app/(article)/layout.tsx
@@ -3,6 +3,19 @@ import { Header } from '@/components/header/article'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <script>
+        {`
+              MathJax = {
+                tex: {
+                  inlineMath: [['$', '$'], ['\\(', '\\)']],
+                  displayMath: [['$$', '$$'], ['\\\\[', '\\\\]']]
+                },
+                svg: {
+                  fontCache: 'global'
+                }
+              };
+            `}
+      </script>
       <script
         async
         src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.js'

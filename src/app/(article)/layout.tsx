5ef23c64d325169e2c6f7f707142f61dda806dfa
ZhuoQuanFan <fanzhuoquan@gmail.com>
2024-10-20 12:12:05 +0800

    update latex

--- src/app/(article)/layout.tsx
+++ src/app/(article)/layout.tsx
@@ -3,9 +3,12 @@ import { Header } from '@/components/header/article'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      {/* eslint-disable-next-line @next/next/no-unwanted-polyfillio */}
+      <script async src='https://polyfill.io/v3/polyfill.min.js?features=es6' />
       <script
         async
-        src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML'
+        id='MathJax-script'
+        src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'
       />
       <script>
         {`

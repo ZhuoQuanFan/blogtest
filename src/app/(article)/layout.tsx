77239c1f953541f33e2138bb0b657993c001afe1
ZhuoQuanFan <fanzhuoquan@gmail.com>
2024-10-20 12:08:02 +0800

    update summary

--- src/app/(article)/layout.tsx
+++ src/app/(article)/layout.tsx
@@ -7,6 +7,23 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         async
         src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML'
       />
+      <script>
+        {`
+              MathJax = {
+                tex: {
+                  inlineMath: [['$', '$'], ['\\(', '\\)']]
+                },
+                svg: {
+                  fontCache: 'global'
+                }
+              };
+            `}
+      </script>
+
+      {/*<script*/}
+      {/*  async*/}
+      {/*  src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.js'*/}
+      {/*/>*/}
       <Header />
 
       {children}
